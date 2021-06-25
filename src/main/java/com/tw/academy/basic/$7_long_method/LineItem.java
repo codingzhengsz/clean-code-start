fa5ee41eaaeec2370e646c5caba23b4d2ab58d4d
sutton <codingzhengsz@163.com>
2021-06-25 22:22:14 +0800

    [Martin] refactor:

--- src/main/java/com/tw/academy/basic/$7_long_method/LineItem.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/LineItem.java
@@ -1,30 +1,35 @@
 package com.tw.academy.basic.$7_long_method;
 
 public class LineItem {
-	private String description;
-	private double price;
-	private int quantity;
+    private String description;
+    private double price;
+    private int quantity;
 
-	public LineItem(String description, double p, int quantity) {
-		super();
-		this.description = description;
-		this.price = p;
-		this.quantity = quantity;
-	}
+    public LineItem(String description, double p, int quantity) {
+        super();
+        this.description = description;
+        this.price = p;
+        this.quantity = quantity;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    public double getPrice() {
+        return price;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
     double totalAmount() {
         return price * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return this.getDescription() + "\t" + this.getPrice() + "\t" + this.getQuantity() + "\t" + this.totalAmount() + "\n";
     }
 }
--- src/main/java/com/tw/academy/basic/$7_long_method/README.md
+++ src/main/java/com/tw/academy/basic/$7_long_method/README.md
@@ -2,3 +2,4 @@
 2. Naming, rename the variable in printReceipt method
 3. Magic Number, extract Magic String "======Printing Orders======\n"
 4. Dead Code, Commented statement
+5. Extract Method, Add toString method to LineItem, and replace the appended content in for loop 
